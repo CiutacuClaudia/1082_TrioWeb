d4750cd6a460f86d07af173eaff604297341e0f6
CiutacuClaudia <ciutacuclaudia20@stud.ase.ro>
2022-12-16 02:52:46 +0200

    actualizare contulMeu+Frigider+OferaProdus

--- frontend/src/components/ContulMeu.js
+++ frontend/src/components/ContulMeu.js
@@ -1,10 +1,40 @@
-import React from 'react'
-// import {Link} from 'react-router-dom'
+import React from "react";
 function ContulMeu() {
-    return (
-        <>
-        <h1>Contul meu</h1>
-        </>
-    )
+  return (
+    <>
+      <h1>Contul meu</h1>
+      <form className="contulMeu">
+        <div>
+          <label htmlFor="nume">Nume</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="prenume">Prenume</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="adresa">Adresa</label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="grup">Grup preferential</label>
+          <select name="grup">
+            <option value="vegetarian">Vegetarian</option>
+            <option value="pofticios de de toate">Iubitor de zacusca</option>
+            <option value="vegan">Vegan</option>
+            <option value="iubitor de dulciuri">Iubitor de dulciuri</option>
+            <option value="carnivor">Carnivor</option>
+          </select>
+        </div>
+        <div className="buton">
+          <button type="button">Creeaza cont !</button>
+        </div>
+      </form>
+    </>
+  );
 }
 export default ContulMeu;
--- frontend/src/components/ProduseDisponibile.js
+++ frontend/src/components/ProduseDisponibile.js
@@ -1,5 +1,4 @@
 import React from 'react'
-// import {Link} from 'react-router-dom'
 function ProduseDisponibile() {
     return (
         <h1>Produse Disponibile</h1>
